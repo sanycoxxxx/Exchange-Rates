7186563cf041812bbdd270ef52ffe190cbcc1113
master <sanycoxxxx@gmail.com>
2022-01-27 22:15:06 +0200

    Add json and php

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use script';
+
+const inputMain = document.querySelector('#mainRateInput'),
+inputSecond = document.querySelector('#secondRateInput');
+
+// class exhange {
+//     constructor
+// }
+
+inputMain.addEventListener('input', () => {
+    const request = new XMLHttpRequest;
+    request.open('GET','content.json', );
+    request.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputSecond.value = +inputMain.value / data.current.usd;
+        }else {
+            inputSecond.value = "Что то пошло не так";
+        }
+    });
+    
+})
